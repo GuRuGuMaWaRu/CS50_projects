589f4df7399339c85e856e99a9ea8b0a66ad6627
Peter Krevenets <peter.k.nets@gmail.com>
2017-09-18 15:27:42 +0300

    Get total change, ensure that it is correct, and convert into cents

--- pset6/greedy.py
+++ pset6/greedy.py
@@ -1,7 +1,15 @@
 import cs50
 
 def main():
+    print("O hai!")
     # input how much change is owned
+    while(True):
+        print("How much change is owed?")
+        change_in_cents = round(cs50.get_float() * 100)
+        if (change_in_cents > 0):
+            break
+
+    print("Total change: {}".format(change_in_cents))
     # convert dollars into cents
     # declare a variable to count coins
     # using modulus and divisions calculate required coins starting from the largest denomination
