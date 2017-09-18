8e84c2910eef8e289a448ab1d69a4a073078dc6b
Peter Krevenets <peter.k.nets@gmail.com>
2017-09-18 15:03:11 +0300

    Add a greedy,py file and write pseudocode

---
+++ pset6/greedy.py
@@ -0,0 +1,10 @@
+import cs50
+
+def main():
+    # input how much change is owned
+    # convert dollars into cents
+    # declare a variable to count coins
+    # using modulus and divisions calculate required coins starting from the largest denomination
+
+if __name__ == "__main__":
+    main()
