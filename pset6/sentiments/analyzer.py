df0d132cd815e4b544ca43d4f81aa02ff21592a8
Peter Krevenets <peter.k.nets@gmail.com>
2017-09-20 14:31:26 +0300

    Implement Init and Analyze functions in analyzer.py

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -5,11 +5,38 @@ class Analyzer():
 
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
+        
+        # declare sets for positive and negative words
+        self.positives = set()
+        self.negatives = set()
 
-        # TODO
+        # load positive words
+        with open(positives) as lines:
+            for line in lines:
+                if not line.startswith(";") and not line.startswith(" "):
+                    self.positives.add(line.strip())
+
+        # load negative words
+        with open(negatives) as lines:
+            for line in lines:
+                if not line.startswith(";") and not line.startswith(" "):
+                    self.negatives.add(line.strip())
+
+        self.tokenizer = nltk.tokenize.TweetTokenizer()
+        self.score = 0
+
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
-        # TODO
-        return 0
+        tokens = self.tokenizer.tokenize(text)
+
+        for token in tokens:
+            token = token.lower()
+
+            if token in self.positives:
+                self.score += 1
+            elif token in self.negatives:
+                self.score -= 1
+
+        return self.score
