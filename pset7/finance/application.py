51f793bd996f540075861d97540379c937295345
Peter Krevenets <peter.k.nets@gmail.com>
2017-09-23 16:06:53 +0300

    Show aplogy if either password or password confirmation is not provided

--- pset7/finance/application.py
+++ pset7/finance/application.py
@@ -101,13 +101,21 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user."""
-    
+
     # if user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         # ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username")
+        
+        # ensure password was submitted
+        if not request.form.get("password"):
+            return apology("must provide password")
+
+        # ensure password confirmation was submitted
+        if not request.form.get("password_confirmation"):
+            return apology("must provide password confirmation")
 
         #redirect user to home page
         return redirect(url_for("index"))
@@ -115,7 +123,6 @@ def register():
     # if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
-    return apology("TODO")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
